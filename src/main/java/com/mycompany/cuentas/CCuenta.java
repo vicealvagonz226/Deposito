fdec94575777a0871ebd34b13663e128f82b5407
vicealvagonz226 <vicente.gonzalvarez@gmail.com>
2025-02-08 17:41:50 +0000

    Se hace el código más legible, eliminando espacios en blanco y corrigiendo la indentación de la sintaxis

--- src/main/java/com/mycompany/cuentas/CCuenta.java
+++ src/main/java/com/mycompany/cuentas/CCuenta.java
@@ -2,40 +2,35 @@ package com.mycompany.cuentas;
 
 public class CCuenta {
 
-
     private String nombre;
     private String cuenta;
     private double saldo;
-    private double tipoInterés;
+    private double tipoInteres;
 
-    public CCuenta()
-    {
+    public CCuenta() {
+    
+    }
+    
+    public CCuenta(String nom, String cue, double sal, double tipo) {
+        nombre = nom;
+        cuenta = cue;
+        saldo = sal;
     }
 
-    public CCuenta(String nom, String cue, double sal, double tipo)
-    {
-        nombre =nom;
-        cuenta=cue;
-        saldo=sal;
-    }
-
-    public double estado()
-    {
+    public double estado() {
         return saldo;
     }
 
-    public void ingresar(double cantidad) throws Exception
-    {
-        if (cantidad<0)
+    public void ingresar(double cantidad) throws Exception {
+        if (cantidad < 0)
             throw new Exception("No se puede ingresar una cantidad negativa");
         saldo = saldo + cantidad;
     }
 
-    public void retirar(double cantidad) throws Exception
-    {
+    public void retirar(double cantidad) throws Exception {
         if (cantidad <= 0)
             throw new Exception ("No se puede retirar una cantidad negativa");
-        if (estado()< cantidad)
+        if (estado() < cantidad)
             throw new Exception ("No se hay suficiente saldo");
         saldo = saldo - cantidad;
     }
@@ -66,10 +61,10 @@ public class CCuenta {
     }
     
     public double getTipoInteres() {
-        return tipoInterés;
+        return tipoInteres;
     }
     
-    public void setTipoInteres(double tipoInterés) {
-        this.tipoInterés = tipoInterés;
+    public void setTipoInteres(double tipoInteres) {
+        this.tipoInteres = tipoInteres;
     }
 }
